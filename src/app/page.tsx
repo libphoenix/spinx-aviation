bc5a82ac677c06fbeaa3b0a70cd360d2fa2b5353
Deepak Patel <dp@Deepaks-MacBook-Air.local>
2025-07-19 11:01:52 +0530

    feat: updated tmp site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[url('/aeroplane.png')] bg-cover bg-center h-96">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-100">
-        <h1 className="uppercase w-100 text-center font-bold text-4xl">Spinx Aviation</h1>
-        <p className="w-100 text-center">website under construction</p>
-        <img
-          className="mx-auto"
-          src="/road.png"
-          alt="work in progress"
-          width={100}
-          height={100}
-        />
+        <h1 className="w-100 text-center font-semibold text-3xl">We are comming soon</h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
